584b6688ee24bf4dca92f73cbb4a0f1427ec7bed
WillVill <bech.william2@gmail.com>
2017-02-07 18:47:16 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------");
+        System.out.println("Please enter 3 digits");
+        System.out.println("---------------------------------");
+        while (true) {
+            try {
+                System.out.print("Input: ");
+                Scanner sc = new Scanner(System.in);
+                int s1 = sc.nextInt();
+                int s2 = sc.nextInt();
+                int s3 = sc.nextInt();
+
+                new Triangle(s1, s2, s3);
+                
+            } catch (Exception e) {
+                System.out.println("Please enter digits");
+            }
+        }
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,28 @@
+/**
+ * Created by williambech on 2/7/17.
+ */
+public class Triangle {
+
+
+
+    public Triangle(int a, int b, int c) {
+
+        if ((a > 0 && b > 0 && c > 0) && (a + b > c && a + c > b && b + c > a)) {
+            if (a == b && b == c) {
+                System.out.println("That is an equilateral triangle");
+            }
+
+            else if ((a == b && a != c) || (b == c && b != a) || (c == a && c != b)) {
+                System.out.println("That is an isosceles triangle");
+            }
+
+            else if (a != b && b != c && a != c) {
+                System.out.println("That is an scalene triangle");
+            }
+
+        } else {
+            System.out.println("Those measurements don't form a triangle");
+        }
+        System.out.println("------------------------------");
+    }
+}
